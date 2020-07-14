4b80bb980ba2936891b40c4eba1e3ca2a9a135aa
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-07-14 14:26:39 +0900

    Dev add 10yearafter (#11)
    * Update storybuilder(v0.5.9-22)
    
    * Add 10years after production
    
    * Update travis
    
    * Update v0.8.0 [ci skip]

--- src/main.py
+++ src/main.py
@@ -8,8 +8,11 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('storybuilder')
 from storybuilder.builder.world import World
-from src.config import PERSONS, STAGES, DAYS, TIMES, ITEMS, WORDS, RUBIS
-from src.story_1to10.story007 import ch007_windandbubble
+from storybuilder.assets import basic
+from storybuilder.assets import common_rubi
+from config import ASSET
+# import scenes
+from story_1to10 import Story008
 
 
 ################################################################
@@ -21,17 +24,50 @@ from src.story_1to10.story007 import ch007_windandbubble
 #   5.悲しみは雨に似ている
 #   6.涙の大河
 #   7.風とシャボン玉
+#   8.十年後の世界の君に
 #
 ################################################################
 
 
+# Constant
+TITLE = "原稿用紙の為の作品集"
+MAJOR, MINOR, MICRO = 8, 0, 0
+COPY = "原稿用紙は、物語をいつも待っている"
+ONELINE = "原稿用紙三枚（約1200字程度）の掌編集。ジャンルも作風もバラバラです"
+OUTLINE = "凪司工房が昔書いた掌編集から選んだ作品をリメイクし、約一冊分になるように作品を集めたものです"
+THEME = "バラエティ"
+GENRE = "各作品による"
+TARGET = "20-50台（男女）"
+SIZE = "〜1.2K"
+CONTEST_INFO = ""
+CAUTION = ""
+NOTE = ""
+SITES = ["エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム"]
+RELEASED = (4, 15, 2020)
+
+
 
 def main(): # pragma: no cover
-    w = World.create_world('原稿用紙の為の作品集１')
-    w.config.set_outline('原稿用紙三枚程度の短編を収めたもの')
-    w.db.build_db(PERSONS, STAGES, DAYS, TIMES, ITEMS, WORDS, RUBIS)
+    w = World.create_world(f"{TITLE}")
+    w.config.set_version(MAJOR, MINOR, MICRO)
+    w.db.set_from_asset(basic.ASSET)
+    w.db.set_from_asset(common_rubi.ASSET)
+    w.db.set_from_asset(ASSET)
+    # spec
+    w.config.set_copy(f"{COPY}")
+    w.config.set_oneline(f"{ONELINE}")
+    w.config.set_outline(f"{OUTLINE}")
+    w.config.set_theme(f"{THEME}")
+    w.config.set_genre(f"{GENRE}")
+    w.config.set_target(f"{TARGET}")
+    w.config.set_size(f"{SIZE}")
+    w.config.set_contest_info(f"{CONTEST_INFO}")
+    w.config.set_caution(f"{CAUTION}")
+    w.config.set_note(f"{NOTE}")
+    w.config.set_sites(*SITES)
+    w.config.set_released(*RELEASED)
     return w.run(
-            ch007_windandbubble(w),
+            Story008.ch_story008(w),
             )
 
 
